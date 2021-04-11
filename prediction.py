041097587a24d6ad5fc8cd23cc224a35bac9cce4
Quormas <81319657+Quormas@users.noreply.github.com>
2021-04-11 15:17:42 +0500

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import Ridge
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
-
+from sklearn.metrics import r2_score
 
 file = open("data.txt", "r")
 data = file.read().split("\n")
@@ -20,16 +20,21 @@ for i in range(len(data)):
 x = np.array(data_x).reshape((-1, 1))
 y = np.array(data_y)
 
-print(x)
-print(y)
-
 plt.scatter(x, y, color="blue")
 
-poly = make_pipeline(PolynomialFeatures(degree=4), Ridge())
+arr = []
+
+for i in range(6):
+    poly_i = make_pipeline(PolynomialFeatures(degree=i), Ridge())
+    poly_i.fit(x,y)
+    arr.append(r2_score(y, poly_i.predict(x)))
+    
+deg = arr.index(max(arr))
+    
+poly = make_pipeline(PolynomialFeatures(degree=deg), Ridge())
 poly.fit(x,y)
-
 y_poly = poly.predict(x)
-
+    
 plt.plot(x, y_poly, color="red")
 
 pred_x = np.array( [len(x)] ).reshape((-1, 1))
@@ -37,6 +42,6 @@ pred_y = poly.predict(pred_x)
 
 plt.scatter(pred_x, pred_y, color="red")
 
-plt.title(pred_y, fontsize=15)
+plt.title(pred_y[0], fontsize=15)
 
 plt.show()
